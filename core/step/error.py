ff45203e2dbe74795385706762dfc06e6d3fe870
Zhalslar <zsl2936169201@gmail.com>
2026-01-17 12:58:20 +0800

    up

--- core/step/error.py
+++ core/step/error.py
@@ -14,7 +14,7 @@ class ErrorStep(BaseStep):
     def __init__(self, config: PluginConfig):
         super().__init__(config)
         self.cfg = config.error
-        self.admin_id = config.admins_id[0] if config.admins_id else None
+        self.admins_id = config.admins_id
 
     def _find_hit_keyword(self, text: str) -> str | None:
         """
@@ -30,16 +30,18 @@ class ErrorStep(BaseStep):
         """
         转发消息给设定的会话
         """
+        chain = MessageChain([Plain(ctx.plain)])
+        context = self.plugin_config.context
         if self.cfg.forward_umo == "admin":
-            session = copy.copy(ctx.event.session)
-            if self.admin_id:
-                session.session_id = self.admin_id
+            if not self.admins_id:
+                raise Exception("未配置管理员ID, 无法转发报错信息")
+
+            for admin_id in self.admins_id:
+                session = copy.copy(ctx.event.session)
+                session.session_id = admin_id
+                await context.send_message(session, chain)
         else:
-            session = self.cfg.forward_umo
-        await self.plugin_config.context.send_message(
-            session,
-            MessageChain([Plain(ctx.plain)]),
-        )
+            await context.send_message(self.cfg.forward_umo, chain)
 
     async def handle(self, ctx: OutContext) -> StepResult:
         """
@@ -51,7 +53,7 @@ class ErrorStep(BaseStep):
             return StepResult()
         msg = f"命中报错关键词 {hit_word}"
 
-        if  self.cfg.forward_umo:
+        if self.cfg.forward_umo:
             await self._forward_to_admin(ctx)
             msg += f"，已转发至{self.cfg.forward_umo}"
 
@@ -60,4 +62,3 @@ class ErrorStep(BaseStep):
         msg += f"，原消息替换为 {self.cfg.custom_msg}"
 
         return StepResult(msg=msg)
-
